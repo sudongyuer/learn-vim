e40780b3170c6785c937faae0648424b612c66cf
sudongyu <976499226@qq.com>
2022-06-03 20:31:11 +0800

    chore: update config.ts

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  lang: 'en-US',
+  title: 'Vim通关小册',
+  description: 'Vite & Vue powered static site generator.',
+  lastUpdated: true,
+  base:"/learn-vim/",
+
+  themeConfig: {
+    repo: 'sudongyuer/learn-vim',
+    docsDir: 'docs',
+    docsBranch: 'main',
+    editLinks: false,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    algolia: {
+      appId: '8J64VVRP8K',
+      apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+      indexName: 'vitepress'
+    },
+
+    nav: [
+      { text: 'Start', link: '/guide', activeMatch: '^/$|^/guide/' },
+      {
+        text: 'About ME',
+        link: 'https://github.com/sudongyuer'
+      }
+    ],
+
+    sidebar: {
+      '/guide/': getGuideSidebar(),
+      '/': getGuideSidebar()
+    }
+  }
+})
+
+function getGuideSidebar() {
+  return [
+    {
+      text: '开始我们Vim学习吧～👀',
+       link:''
+    },
+    { text: '什么是Vim', link: '/guide/什么是Vim' },
+    { text: '安装Vim', link: '/guide/安装Vim' },
+    { text: 'Vim练习第一天', link: '/guide/Vim练习第一天'}
+  ]
+}
