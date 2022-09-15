0904091abcd23bed66fa015076cab49713449042
Frozen FIsh <976499226@qq.com>
2022-09-15 22:46:11 +0800

    chore: update

--- src/vim-day01.js
+++ src/vim-day01.js
@@ -89,14 +89,14 @@ function sayHello (){
 const arr = [1,2,3,4,5,6,7,8,9,10]
 
 sayhello()
+SAYHELLO()
+SAYHELLO()
 sayhello()
+SAYHELLO()
+SAYHELLO()
 sayhello()
-sayhello()
-sayhello()
-sayhello()
-sayhello()
-sayhello()
-sayhello()
+SAYHELLO()
+SAYHELLO()
 
 vnode
 vnode 
